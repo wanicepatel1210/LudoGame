3df9d531bf303c018b3d451d3ec86a5a387b61ec
Vishalkumar <vsojitra@mail.sfsu.edu>
2019-07-26 21:21:13 -0700

    image-upload.js change as per harsham comment

--- application/views/js/image-upload.js
+++ application/views/js/image-upload.js
@@ -1,22 +1,16 @@
 $(document).ready(function() {
-
-    var readURL = function(input) {
-        if (input.files && input.files[0]) {
-            var reader = new FileReader();
-
-            reader.onload = function (e) {
-                $('.profile-pic').attr('src', e.target.result);
-            }
-    
-            reader.readAsDataURL(input.files[0]);
-        }
-    }
-    
-
     $(".file-upload").on('change', function(){
-        readURL(this);
+      if (this.files && this.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              $('.profile-pic').attr('src', e.target.result);
+          }
+
+          reader.readAsDataURL(input.files[0]);
+      }
     });
-    
+
     $(".upload-button").on('click', function() {
        $(".file-upload").click();
     });
